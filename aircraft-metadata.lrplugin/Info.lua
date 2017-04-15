fbad983a5017ce6ad4f6243432b9f9aba76e2b4b
aviationphoto <oliver@McIntosh.local>
2017-04-15 08:50:42 +0200

    set version to v1.4.1 DEV

--- aircraft-metadata.lrplugin/Info.lua
+++ aircraft-metadata.lrplugin/Info.lua
@@ -18,9 +18,9 @@ along with LR Aircraft Metadata.  If not, see <http://www.gnu.org/licenses/>.
 ------------------------------------------------------------------------------]]
 return {
 	LrSdkVersion = 6.0,
-	VERSION = {major=1, minor=3, revision=2,},
+	VERSION = {major=1, minor=4, revision=1,},
 
-	LrPluginName = 'Aircraft Metadata',
+	LrPluginName = 'Aircraft Metadata DEV',
 	LrToolkitIdentifier = 'ch.aviationphoto.aircraftmetadata',
 
 	LrMetadataProvider = 'MetadataProvider.lua',
