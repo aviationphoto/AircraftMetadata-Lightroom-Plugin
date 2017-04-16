70df3335db9fd25d5d152cf942440d6478dd1000
aviationphoto <oliver@McIntosh.local>
2017-04-16 10:20:22 +0200

    write preferences to log

--- aircraft-metadata.lrplugin/Utilities.lua
+++ aircraft-metadata.lrplugin/Utilities.lua
@@ -36,22 +36,28 @@ LrLogger = import 'LrLogger'('AircraftMetadata')
 -- start logger
 function startLogger(functionName)
 	local logPath, success, reason
-	-- check if logging enabled
-	if LrPrefs.prefFlagLogging then
-		LrLogger:enable('logfile')
-		-- clear old logfile
-		logPath = LrPathUtils.child(LrPathUtils.getStandardFilePath('documents'), 'AircraftMetadata.log')
-		if LrFileUtils.exists(logPath) then
-			success, reason = LrFileUtils.delete(logPath)
-			if not success then
-				LrLogger:error('error deleting existing logfile!'..reason)
-			end
-		end
+
+	-- lookup prefFlagLogging
+	if (LrPrefs.prefFlagLogging == nil) then
+		LrErrors.throwUserError('Please set Logging preference')
 	else
-		LrLogger:disable()
+		-- check if logging enabled
+		if LrPrefs.prefFlagLogging then
+			LrLogger:enable('logfile')
+			-- clear old logfile
+			logPath = LrPathUtils.child(LrPathUtils.getStandardFilePath('documents'), 'AircraftMetadata.log')
+			if LrFileUtils.exists(logPath) then
+				success, reason = LrFileUtils.delete(logPath)
+				if not success then
+					LrLogger:error('error deleting existing logfile!'..reason)
+				end
+			end
+		else
+			LrLogger:disable()
+		end
+		LrLogger:info('>>>> running '..functionName)
+		LrLogger:info('Lightroom version: '..LrApplication.versionString()..' on '..LrSystemInfo.summaryString())
 	end
-	LrLogger:info('>>>> running '..functionName)
-	LrLogger:info('Lightroom version: '..LrApplication.versionString()..' on '..LrSystemInfo.summaryString())
 end
 
 ------- setPhotoLogFilename() ------------------------------------------------
@@ -71,52 +77,84 @@ end
 ------- loadPrefs() -----------------------------------------------------------
 -- load saved preferences
 function loadPrefs()
+	LrLogger:info('-- loading preferences ------------------------')
+	-- lookup prefFlagOverwrite
+	if (LrPrefs.prefFlagOverwrite == nil) then
+		LrErrors.throwUserError('Please set Overwrite preference')
+	else
+		LrLogger:info('prefFlagOverwrite:      '..tostring(LrPrefs.prefFlagOverwrite))
+	end
 	-- lookup KeywordRegNotFound
 	if (LrPrefs.prefKeywordRegNotFound == nil or LrPrefs.prefKeywordRegNotFound == '') then
 		LrErrors.throwUserError('Please set KeywordRegNotFound')
+	else
+		LrLogger:info('prefKeywordRegNotFound: '..LrPrefs.prefKeywordRegNotFound)
 	end
 	-- lookup KeywordWrongReg
 	if (LrPrefs.prefKeywordWrongReg == nil or LrPrefs.prefKeywordWrongReg == '') then
 		LrErrors.throwUserError('Please set KeywordWrongReg')
+	else
+		LrLogger:info('prefKeywordWrongReg:    '..LrPrefs.prefKeywordWrongReg)
 	end
 	-- lookup URL
 	if (LrPrefs.prefLookupUrl == nil or LrPrefs.prefLookupUrl == '') then
 		LrErrors.throwUserError('Please set URL for lookup')
+	else
+		LrLogger:info('prefLookupUrl:          '..LrPrefs.prefLookupUrl)
 	end
 	-- lookup RegistrationToken1
 	if (LrPrefs.prefRegistrationToken1 == nil or LrPrefs.prefRegistrationToken1 == '') then
 		LrErrors.throwUserError('Please set registration token 1')
+	else
+		LrLogger:info('prefRegistrationToken1: '..LrPrefs.prefRegistrationToken1)
 	end
 	-- lookup RegistrationToken2
 	if (LrPrefs.prefRegistrationToken2 == nil or LrPrefs.prefRegistrationToken2 == '') then
 		LrErrors.throwUserError('Please set registration token 2')
+	else
+		LrLogger:info('prefRegistrationToken2: '..LrPrefs.prefRegistrationToken2)
 	end
 	-- lookup AirlineToken1
 	if (LrPrefs.prefAirlineToken1 == nil or LrPrefs.prefAirlineToken1 == '') then
 		LrErrors.throwUserError('Please set airline token 1')
+	else
+		LrLogger:info('prefAirlineToken1:      '..LrPrefs.prefAirlineToken1)
 	end
 	-- lookup AirlineToken2
 	if (LrPrefs.prefAirlineToken2 == nil or LrPrefs.prefAirlineToken2 == '') then
 		LrErrors.throwUserError('Please set airline token 2')
+	else
+		LrLogger:info('prefAirlineToken2:      '..LrPrefs.prefAirlineToken2)
 	end
 	-- lookup AircraftToken1
 	if (LrPrefs.prefAircraftToken1 == nil or LrPrefs.prefAircraftToken1 == '') then
 		LrErrors.throwUserError('Please set aircraft token 1')
+	else
+		LrLogger:info('prefAircraftToken1:     '..LrPrefs.prefAircraftToken1)
 	end
 	-- lookup AircraftToken2
 	if (LrPrefs.prefAircraftToken2 == nil or LrPrefs.prefAircraftToken2 == '') then
 		LrErrors.throwUserError('Please set aircraft token 2')
+	else
+		LrLogger:info('prefAircraftToken2:     '..LrPrefs.prefAircraftToken2)
 	end
 	-- lookup ManufacturerToken1
 	if (LrPrefs.prefManufacturerToken1 == nil or LrPrefs.prefManufacturerToken1 == '') then
 		LrErrors.throwUserError('Please set manufacturer token 1')
+	else
+		LrLogger:info('prefManufacturerToken1:  '..LrPrefs.prefManufacturerToken1)
 	end
 	-- lookup ManufacturerToken2
 	if (LrPrefs.prefManufacturerToken2 == nil or LrPrefs.prefManufacturerToken2 == '') then
 		LrErrors.throwUserError('Please set manufacturer token 2')
+	else
+		LrLogger:info('prefManufacturerToken2:  '..LrPrefs.prefManufacturerToken2)
 	end
 	-- lookup Marker for Successful Search
 	if (LrPrefs.prefSuccessfulSearch == nil or LrPrefs.prefSuccessfulSearch == '') then
 		LrErrors.throwUserError('Please set marker for successful search')
+	else
+		LrLogger:info('prefSuccessfulSearch:   '..LrPrefs.prefSuccessfulSearch)
 	end
+	LrLogger:info('-- loading preferences done -------------------')
 end
