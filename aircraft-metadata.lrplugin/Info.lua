e3eb3b5842fccb93b6040df98c1ecde602bf280d
aviationphoto <aviationphoto@users.noreply.github.com>
2020-02-23 13:59:24 +0100

    show Message on start

--- aircraft-metadata.lrplugin/Info.lua
+++ aircraft-metadata.lrplugin/Info.lua
@@ -18,7 +18,7 @@ along with LR Aircraft Metadata.  If not, see <http://www.gnu.org/licenses/>.
 ------------------------------------------------------------------------------]]
 return {
 	LrSdkVersion = 6.0,
-	VERSION = {major=1, minor=5, revision=2,},
+	VERSION = {major=1, minor=5, revision=3,},
 
 	LrPluginName = 'Aircraft Metadata DEV',
 	LrToolkitIdentifier = 'ch.aviationphoto.aircraftmetadata',
